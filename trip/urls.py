5cf922fd61bc4ae2ef099bd8999ba7a4777194a4
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-16 15:31:03 +0000

    Added final touches to the applcation

--- trip/urls.py
+++ trip/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomeView, trip_list, TripCreateView, TripDetailView, NoteDetailView, NoteListView, NoteCreateView
+from .views import HomeView, trip_list, TripCreateView, TripDetailView, NoteDetailView, NoteListView, NoteCreateView, NoteUpdateView, NoteDeleteView, TripUpdateView, TripDeleteView
 
 urlpatterns = [
     path("", HomeView.as_view(), name='home'),
@@ -9,5 +9,9 @@ urlpatterns = [
     path("dashboard/note/create", NoteCreateView.as_view(), name='note-create'),
     path("dashboard/trip/create/", TripCreateView.as_view(), name='trip-create'),
     path("dashboard/trip/<int:pk>/", TripDetailView.as_view(), name='trip-detail'),
+    path("dashboard/trip/<int:pk>/update/", TripUpdateView.as_view(), name='trip-update'),
+    path("dashboard/trip/<int:pk>/delete/", TripDeleteView.as_view(), name='trip-delete'),
     path("dashboard/note/<int:pk>/", NoteDetailView.as_view(), name='note-detail'),
+    path("dashboard/note/<int:pk>/update/", NoteUpdateView.as_view(), name='note-update'),
+    path("dashboard/note/<int:pk>/delete/", NoteDeleteView.as_view(), name='note-delete'),
 ]
