3bdc138ab3579652d418be33d7e861593d205df5
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-21 16:01:41 +0000

    prior to deployment changes

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&^m8@8k41ira5hgmpgpex6c%)9chx-bj(ecc*1lbssc*d-iu9s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 
 # Application definition
