0575c299ea42963b437f547ca401c042542c510b
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2023-11-21 16:39:09 +0000

    trying improved signup

--- trip/views.py
+++ trip/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView, DetailView, ListView, UpdateView, DeleteView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
 
 from .models import Trip, Note
@@ -18,8 +19,15 @@ def trip_list(request):
     }
     return render(request, 'trip/trips_list.html', context)
 
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
 class SignupView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
